b873e66218eb6f507a94f244673d58a610bf6016
nyagera <louiscbr@yahoo.com>
2026-09-16 00:34:18 +0200

    Update oauth-protected-resource.js

--- api/oauth-protected-resource.js
+++ api/oauth-protected-resource.js
@@ -4,13 +4,8 @@
 module.exports = function handler(req, res) {
   const proto = req.headers["x-forwarded-proto"] || "https";
   const base = `${proto}://${req.headers.host}`;
-// api/oauth-protected-resource.js
-module.exports = function handler(req, res) {
-  const proto = req.headers["x-forwarded-proto"] || "https";
-  const base = `${proto}://${req.headers.host}`;
   res.status(200).json({
     resource: `${base}/mcp`,
     authorization_servers: [base],
   });
-};;
 };
